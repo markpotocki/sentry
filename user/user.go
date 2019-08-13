81d5ba9e1e6dca5ca559fd9042603c474389232b
Mark <potockimark5@gmail.com>
2019-08-13 16:46:53 -0500

    change User to be an interface

--- user/user.go
+++ user/user.go
@@ -3,7 +3,12 @@ package user
 import "log"
 import "golang.org/x/crypto/bcrypt"
 
-type User struct {
+type User interface {
+	Username() string
+	Pwd() string
+	encodePassword(unencodedPwd string) string
+}
+type MyUser struct {
 	username string
 	password string
 	role     string
@@ -18,20 +23,28 @@ type User struct {
  * 3. Administrator
  */
 
-func MakeUser(username string, unencodedPwd string, email string, name string) *User {
+func (m *MyUser) Username() string {
+	return m.username
+}
+
+func (m *MyUser) Pwd() string {
+	return m.password
+}
+
+func MakeUser(username string, unencodedPwd string, email string, name string) *MyUser {
 	return makeInternalUser(username, unencodedPwd, "USER", email, name)
 }
 
-func MakeModerator(username string, unencodedPwd string, email string, name string) *User {
+func MakeModerator(username string, unencodedPwd string, email string, name string) *MyUser {
 	return makeInternalUser(username, unencodedPwd, "MOD", email, name)
 }
 
-func MakeAdmin(username string, unencodedPwd string, email string, name string) *User {
+func MakeAdmin(username string, unencodedPwd string, email string, name string) *MyUser {
 	return makeInternalUser(username, unencodedPwd, "ADMIN", email, name)
 }
 
-func makeInternalUser(username string, unencodedPwd string, role string, email string, name string) *User {
-	return &User{username, encodePassword(unencodedPwd), role, email, name}
+func makeInternalUser(username string, unencodedPwd string, role string, email string, name string) *MyUser {
+	return &MyUser{username, encodePassword(unencodedPwd), role, email, name}
 }
 
 func encodePassword(unencodedPwd string) string {
