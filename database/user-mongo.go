23cec85b7c73f77c84c60654bd819c2a460b88f3
Mark <potockimark5@gmail.com>
2019-08-15 14:21:44 -0500

    add mongo dependency

--- database/user-mongo.go
+++ database/user-mongo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -53,4 +54,8 @@ func (db *Database) Save(database string, collection string, data ...interface{}
 	}
 
 	fmt.Println("Inserted documents: ", insertR.InsertedIDs)
+}
+
+func (db *Database) Find(database string, collection string, filter bson.D) {
+
 }
