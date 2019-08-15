018652764fcec9e40e36a3cd7d9aa510b84ede66
Mark <potockimark5@gmail.com>
2019-08-15 17:18:21 -0500

    add db save to MakeUser

--- user/user.go
+++ user/user.go
@@ -1,7 +1,14 @@
 package user
 
-import "log"
-import "golang.org/x/crypto/bcrypt"
+import (
+	"context"
+	"fmt"
+	"idendity-provider/database"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User interface {
 	Username() string
@@ -9,11 +16,11 @@ type User interface {
 	encodePassword(unencodedPwd string) string
 }
 type MyUser struct {
-	username string
-	password string
-	role     string
-	email    string
-	name     string
+	UserID   string
+	Password string
+	Role     string
+	Email    string
+	Name     string
 }
 
 /*
@@ -24,15 +31,36 @@ type MyUser struct {
  */
 
 func (m *MyUser) Username() string {
-	return m.username
+	return m.UserID
 }
 
 func (m *MyUser) Pwd() string {
-	return m.password
+	return m.Password
 }
 
 func MakeUser(username string, unencodedPwd string, email string, name string) *MyUser {
-	return makeInternalUser(username, unencodedPwd, "USER", email, name)
+	u := makeInternalUser(username, unencodedPwd, "USER", email, name)
+	c := database.Mongo.Database("user").Collection("USER")
+	insertR, err := c.InsertMany(context.TODO(), []interface{}{u})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted documents: ", insertR.InsertedIDs)
+	return u
+}
+
+func FindUserByUsername(username string) *MyUser {
+	var result MyUser
+	filter := bson.D{{"userid", username}}
+	c := database.Mongo.Database("user").Collection("USER")
+	log.Printf("searching key [%s] with value [%s]\n", "userid", username)
+	err := c.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &result
 }
 
 func MakeModerator(username string, unencodedPwd string, email string, name string) *MyUser {
@@ -44,10 +72,14 @@ func MakeAdmin(username string, unencodedPwd string, email string, name string) 
 }
 
 func makeInternalUser(username string, unencodedPwd string, role string, email string, name string) *MyUser {
-	return &MyUser{username, encodePassword(unencodedPwd), role, email, name}
+	hash, err := bcrypt.GenerateFromPassword([]byte(unencodedPwd), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return &MyUser{username, string(hash), role, email, name}
 }
 
-func encodePassword(unencodedPwd string) string {
+func EncodePassword(unencodedPwd string) string {
 	hash, err := bcrypt.GenerateFromPassword([]byte(unencodedPwd), bcrypt.DefaultCost)
 	if err != nil {
 		log.Println(err)
--- user/user_test.go
+++ user/user_test.go
@@ -1,16 +1,24 @@
 package user
 
-import "testing"
+import (
+	"testing"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func testEncodePwd(t *testing.T) {
 	pwd := "thisisbreakable"
-	epwd := encodePassword(pwd)
+	epwd := EncodePassword(pwd)
 
 	// no errors, test if valid
-	e2pwd := encodePassword(pwd)
+	e2pwd := EncodePassword(pwd)
 
 	if epwd != e2pwd {
 		t.Log("hashes of password did not match")
+		t.Fail()
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(epwd), []byte(pwd)); err != nil {
 		t.Fail()
 	}
 }
