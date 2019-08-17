7571f3d029523662645b0747b0365857c0ce1f0f
Mark <potockimark5@gmail.com>
2019-08-16 19:00:22 -0500

    add session support

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"idendity-provider/view"
+	session "idendity-provider/sessions"
+	_ "idendity-provider/sessions/memory"
 	"log"
 	"net/http"
 )
+
+var globalSessions *session.Manager
 
 func main() {
 	fs := http.FileServer(http.Dir("templates/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/login", view.LoginPage)
-	http.HandleFunc("/register", view.RegisterPage)
+	http.HandleFunc("/login", LoginPage)
+	http.HandleFunc("/register", RegisterPage)
 
 	log.Println("Serving content on port 8070.")
 	http.ListenAndServe(":8070", nil)
+}
+
+func init() {
+	globalSessions, _ = session.NewManager("memory", "SESSION_ID", 3600)
+	go globalSessions.GC()
 }
